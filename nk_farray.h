d18d822a2901ecc41ee4573040ba79a588d6d723
Nenad Radulovic <nenad.b.radulovic@gmail.com>
2021-05-08 14:26:27 +0200

    chore:nk_farray, added NK_FARRAY__LENGTH()
    Signed-off-by: Nenad Radulovic <nenad.b.radulovic@gmail.com>

--- nk_farray.h
+++ nk_farray.h
@@ -2,7 +2,9 @@
  * nk_farray.h
  *
  *  Created on: Apr 13, 2021
- *      Author: nenad
+ *      Author: (nbr) nenad.b.radulovic@gmail.com
+ *
+ *  08/05/2021: (nbr) Added NK_FARRAY__SIZE() macro
  */
 
 #ifndef GENERIC_NK_FARRAY_H_
@@ -15,5 +17,8 @@
 
 #define NK_FARRAY__LENGTH(self)                                             \
         (sizeof((self)->items) / sizeof((self)->items[0]))
+
+#define NK_FARRAY__SIZE(self)                                               \
+        NK_FARRAY__LENGTH(self)
 
 #endif /* GENERIC_NK_FARRAY_H_ */
