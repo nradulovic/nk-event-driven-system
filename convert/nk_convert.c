c73431058665b2921f3dd2f32a0f2a7436a78578
Nenad Radulovic <nenad.b.radulovic@gmail.com>
2021-05-08 14:31:13 +0200

    chore:nk_convert, hex functions will convert whole binary byte.
    Signed-off-by: Nenad Radulovic <nenad.b.radulovic@gmail.com>

--- convert/nk_convert.c
+++ convert/nk_convert.c
@@ -2,7 +2,9 @@
  * nk_convert.c
  *
  *  Created on: Apr 12, 2021
- *      Author: nenad
+ *      Author: (nbr) nenad.b.radulovic@gmail.com
+ *
+ *  08/05/2021: (nbr) Hex functions will convert whole binary byte.
  */
 
 #include <string.h>
@@ -19,10 +21,10 @@ struct hex_map_to_int
     uint8_t to;
 };
 
-struct nk_convert__hex_to_bin__result
-nk_convert__hex_to_bin(char character)
+static uint8_t
+to_bin(char hex_char)
 {
-    struct nk_convert__hex_to_bin__result result;
+    uint8_t result = UINT8_MAX;
 
     static const struct hex_map_to_int_array
         NK_FARRAY__T(const struct hex_map_to_int, 16u)
@@ -30,41 +32,55 @@ nk_convert__hex_to_bin(char character)
                     { '7', 7 }, { '8', 8 }, { '9', 9 }, { 'a', 10 }, { 'b', 11 }, { 'c', 12 }, { 'd', 13 }, { 'e', 14 },
                     { 'f', 15 } } };
 
-    result.error = NK_ERROR__DATA_INVALID;
-    result.value = 0u;
-
     for (size_t i = 0u; i < NK_FARRAY__LENGTH(&hex_map); i++) {
-        if (hex_map.items[i].from == nk_char__lower(character)) {
-            result.error = NK_ERROR__OK;
-            result.value = hex_map.items[i].to;
+        if (hex_map.items[i].from == nk_char__lower(hex_char)) {
+            result = hex_map.items[i].to;
             break;
         }
     }
     return result;
 }
 
-struct nk_convert__bin_to_hex__result
+struct nk_result__u8
+nk_convert__hex_to_bin(struct nk_convert__hex hex)
+{
+    struct nk_result__u8 result;
+    uint8_t lsb;
+    uint8_t msb;
+
+    result.error = NK_ERROR__DATA_INVALID;
+    result.value = 0u;
+
+    lsb = to_bin(hex.lsb);
+    if (lsb == UINT8_MAX) {
+        return result;
+    }
+    msb = to_bin(hex.msb);
+    if (msb == UINT8_MAX) {
+        return result;
+    }
+    result.error = NK_ERROR__OK;
+    result.value = (uint8_t) ((msb << 4u) | lsb);
+    return result;
+}
+
+struct nk_convert__hex
 nk_convert__bin_to_hex(uint8_t byte)
 {
-    struct nk_convert__bin_to_hex__result result;
+    struct nk_convert__hex result;
 
     static const struct bin_map_to_char_array
         NK_FARRAY__T(const char, 16u)
     bin_map = { .items = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' } };
-    if (byte > NK_FARRAY__LENGTH(&bin_map)) {
-        result.error = NK_ERROR__DATA_OVF;
-        result.value = 0u;
-    } else {
-        result.error = NK_ERROR__OK;
-        result.value = bin_map.items[byte];
-    }
+    result.lsb = bin_map.items[byte & 0xfu];
+    result.msb = bin_map.items[(byte >> 4u) & 0xfu];
     return result;
 }
 
-struct nk_convert__str_to_u32__result
+struct nk_result__u32
 nk_convert__str_to_u32(const struct nk_string *string)
 {
-    struct nk_convert__str_to_u32__result result;
+    struct nk_result__u32 result;
     char null_terminated[1024];
     unsigned long ul;
 
@@ -83,7 +99,7 @@ nk_convert__str_to_u32(const struct nk_string *string)
     } else {
         if (ul < UINT32_MAX) {
             result.error = NK_ERROR__OK;
-            result.value = (uint32_t)ul;
+            result.value = (uint32_t) ul;
         } else {
             result.error = NK_ERROR__DATA_OVF;
             result.value = 0u;
--- convert/nk_convert.h
+++ convert/nk_convert.h
@@ -2,11 +2,13 @@
  * nk_convert.h
  *
  *  Created on: Apr 12, 2021
- *      Author: nenad
+ *      Author: (nbr) nenad.b.radulovic@gmail.com
+ *
+ *  08/05/2021: (nbr) Hex functions will convert whole binary byte.
  */
 
-#ifndef GENERIC_NK_CONVERT_H_
-#define GENERIC_NK_CONVERT_H_
+#ifndef GENERIC_CONVERT_NK_CONVERT_H_
+#define GENERIC_CONVERT_NK_CONVERT_H_
 
 #include <stdint.h>
 
@@ -14,18 +16,19 @@
 
 #define NK_ENABLED_CONVERT
 
-struct nk_convert__hex_to_bin__result NK_RESULT__T(uint8_t);
-struct nk_convert__bin_to_hex__result NK_RESULT__T(char);
-struct nk_convert__str_to_u32__result NK_RESULT__T(uint32_t);
+struct nk_convert__hex
+{
+    char msb;
+    char lsb;
+};
 
+struct nk_result__u8
+nk_convert__hex_to_bin(struct nk_convert__hex hex);
 
-struct nk_convert__hex_to_bin__result
-nk_convert__hex_to_bin(char character);
-
-struct nk_convert__bin_to_hex__result
+struct nk_convert__hex
 nk_convert__bin_to_hex(uint8_t byte);
 
-struct nk_convert__str_to_u32__result
+struct nk_result__u32
 nk_convert__str_to_u32(const struct nk_string * string);
 
-#endif /* GENERIC_NK_CONVERT_H_ */
+#endif /* GENERIC_CONVERT_NK_CONVERT_H_ */
