1608581bb07f20fc06396600ffd9ef61422f9170
Nenad Radulovic <nenad.b.radulovic@gmail.com>
2021-05-08 21:44:44 +0200

    feat:added compiler and cpu interfaces
    Signed-off-by: Nenad Radulovic <nenad.b.radulovic@gmail.com>

---
+++ nk_compiler.h
@@ -0,0 +1,15 @@
+/*
+ * nk_compiler.h
+ *
+ *  Created on: May 8, 2021
+ *      Author: nenad
+ */
+
+#ifndef NEON_KIT_GENERIC_NK_COMPILER_H_
+#define NEON_KIT_GENERIC_NK_COMPILER_H_
+
+#include <stdint.h>
+
+uint32_t nk_compiler__instruction_count(void);
+
+#endif /* NEON_KIT_GENERIC_NK_COMPILER_H_ */
---
+++ nk_cpu.h
@@ -0,0 +1,15 @@
+/*
+ * nk_kernel.h
+ *
+ *  Created on: May 8, 2021
+ *      Author: nenad
+ */
+
+#ifndef NEON_KIT_GENERIC_NK_CPU_H_
+#define NEON_KIT_GENERIC_NK_CPU_H_
+
+#include <stdint.h>
+
+uint32_t nk_cpu__instruction_count(void);
+
+#endif /* NEON_KIT_GENERIC_NK_CPU_H_ */
