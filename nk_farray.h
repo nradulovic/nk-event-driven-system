d1a0e314e7b997f581c085c9288e846f16249eef
Nenad Radulovic <nenad.b.radulovic@gmail.com>
2021-05-10 18:34:50 +0200

    feat:nk_farray, added more macros
    Signed-off-by: Nenad Radulovic <nenad.b.radulovic@gmail.com>

--- nk_farray.h
+++ nk_farray.h
@@ -4,19 +4,27 @@
  *  Created on: Apr 13, 2021
  *      Author: (nbr) nenad.b.radulovic@gmail.com
  *
- *  08/05/2021: (nbr) Added NK_FARRAY__SIZE() macro
+ *  08/05/2021: (nbr) Added more macros
  */
 
 #ifndef GENERIC_NK_FARRAY_H_
 #define GENERIC_NK_FARRAY_H_
+
+#include <string.h>
 
 #define NK_FARRAY__T(item_type, item_no)                                    \
 		{                                                                   \
             item_type items[item_no];                                       \
 		}
 
+#define NK_FARRAY__INITIALIZE_EMPTY(self)                                   \
+        memset((self)->items, 0, NK_FARRAY__SIZE(self) * NK_FARRAY__ITEM_SIZE(self))
+
+#define NK_FARRAY__ITEM_SIZE(self)                                          \
+        (sizeof((self)->items[0]))
+
 #define NK_FARRAY__LENGTH(self)                                             \
-        (sizeof((self)->items) / sizeof((self)->items[0]))
+        (sizeof((self)->items) / NK_FARRAY__ITEM_SIZE(self))
 
 #define NK_FARRAY__SIZE(self)                                               \
         NK_FARRAY__LENGTH(self)
