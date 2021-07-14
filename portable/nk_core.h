90d2f2fba555a4a8ccc71701c38094348e799467
Nenad Radulovic <nenad.b.radulovic@gmail.com>
2021-07-14 13:43:15 +0200

    feat: added nk_core__g__sys_freq
    Signed-off-by: Nenad Radulovic <nenad.b.radulovic@gmail.com>

--- portable/nk_core.h
+++ portable/nk_core.h
@@ -5,6 +5,7 @@
  *      Author: (nbr) nenad.b.radulovic@gmail.com
  *  08/05/2021: (nbr) Initial CORE interface definition
  *  13/07/2021: (nbr) Added nk_core__reset
+ *  14/07/2021: (nbr) Added nk_core__g__sys_freq
  */
 /**
  * @file
@@ -24,6 +25,8 @@ extern "C"
 
 #include "generic/portable/nk_compiler.h"
 
+extern uint32_t nk_core__g__sys_freq;
+    
 struct nk_tasker__array;
 
 struct nk_tasker__array * nk_core__tasker_instances(void);
