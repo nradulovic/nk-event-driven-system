987b9f86d4936971b7b00dd8f67710062b68700c
Nenad Radulović <nenad.b.radulovic@gmail.com>
2021-10-11 03:11:21 +0200

    feat: inital implementation of linked list
    Signed-off-by: Nenad Radulović <nenad.b.radulovic@gmail.com>

--- composite/nk_list.h
+++ composite/nk_list.h
@@ -13,6 +13,27 @@ extern "C"
 {
 #endif
 
+struct nk_list
+{
+    struct nk_list * p__next;
+    struct nk_list * p__prev;
+    void * p__object;
+};
+
+static inline void
+nk_list__init(struct nk_list * self, void * object)
+{
+    self->p__next = self;
+    self->p__prev = self;
+    self->p__object = object;
+}
+
+static inline struct nk_list *
+nk_list__next(struct nk_list * self)
+{
+    return self->p__next;
+}
+
 #if defined(__cplusplus)
 }
 #endif
