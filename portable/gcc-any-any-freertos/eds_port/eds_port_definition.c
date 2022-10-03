8f9c53cd3c72b98f8fae6e05497dc295545dbcda
Nenad Radulović <nenad.b.radulovic@gmail.com>
2022-10-03 23:39:46 +0200

    feat: initial implementation for FreeRTOS port
    Signed-off-by: Nenad Radulović <nenad.b.radulovic@gmail.com>

---
+++ portable/gcc-any-any-freertos/eds_port/eds_port_definition.c
@@ -0,0 +1,74 @@
+/*
+ * eds_port_definition.c
+ *
+ *  Created on: Dec 14, 2021
+ *      Author: nenad
+ */
+
+
+#include "eds_port.h"
+#include "eds.h"
+
+#define ALIGN_UP(num, align)                                       \
+        (((num) + (align) - 1u) & ~((align) - 1u))
+
+void
+eds_port__critical_local_lock(struct eds_port__critical_local * critical)
+{
+}
+
+void
+eds_port__critical_local_unlock(struct eds_port__critical_local * critical)
+{
+}
+
+
+void
+eds_port__sleep_local_init(struct eds_port__sleep_local * sleep)
+{
+}
+
+void
+eds_port__sleep_local_wait(struct eds_port__sleep_local * sleep)
+{
+}
+
+void
+eds_port__sleep_local_signal(struct eds_port__sleep_local * sleep)
+{
+}
+
+uint_fast8_t
+eds_port__ffs(uint32_t value)
+{
+    return (uint_fast8_t)(31u - (unsigned)__builtin_clz(value));
+}
+
+size_t
+eds_port__align_up(size_t non_aligned_value)
+{
+    return ALIGN_UP(non_aligned_value, sizeof(void*));
+}
+
+uint32_t
+eds_port__tick_duration_ms(void)
+{
+    return 1u;
+}
+
+uint32_t
+eds_port__tick_from_ms(uint32_t ms)
+{
+    return ms;
+}
+
+void
+eds_port__init(void)
+{
+}
+
+void
+tick_setup(void)
+{
+
+}
---
+++ portable/gcc-any-any-freertos/eds_port/eds_port_definition.h
@@ -0,0 +1,24 @@
+/*
+ * eds_port_definition.h
+ *
+ *  Created on: Dec 14, 2021
+ *      Author: nenad
+ */
+
+#ifndef NEON_KIT_EDS_DOCUMENTATION_EXAMPLES_BLINKY_EDS_PORT_EDS_PORT_DEFINITION_H_
+#define NEON_KIT_EDS_DOCUMENTATION_EXAMPLES_BLINKY_EDS_PORT_EDS_PORT_DEFINITION_H_
+
+struct eds_port__sleep_local
+{
+    int dummy;
+};
+
+struct eds_port__critical_local
+{
+    int dummy;
+};
+
+void
+tick_setup(void);
+
+#endif /* NEON_KIT_EDS_DOCUMENTATION_EXAMPLES_BLINKY_EDS_PORT_EDS_PORT_DEFINITION_H_ */
